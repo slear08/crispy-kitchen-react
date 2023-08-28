7483d2d04cc351c1feb070825a6922004edf9325
slear08 <88441444+slear08@users.noreply.github.com>
2023-08-28 19:23:19 +0800

    ready to deploy

---
+++ src/assets/images/index.js
@@ -0,0 +1,56 @@
+// Author
+export { default as author1 } from './author/author1.jpg';
+export { default as author2 } from './author/author2.jpg';
+
+// Breakfast
+export { default as breakfast1 } from './breakfast/breakfast1.jpg';
+export { default as breakfast2 } from './breakfast/breakfast2.jpg';
+export { default as breakfast3 } from './breakfast/breakfast3.jpg';
+
+// Dinner
+export { default as dinner1 } from './dinner/dinner1.jpg';
+export { default as dinner2 } from './dinner/dinner2.jpg';
+export { default as dinner3 } from './dinner/dinner3.jpg';
+
+// Header
+export { default as header1 } from './header/header1.jpg';
+export { default as header2 } from './header/header2.jpg';
+export { default as header3 } from './header/header3.jpg';
+export { default as header4 } from './header/header4.jpg';
+
+// Lunch
+export { default as lunch1 } from './lunch/lunch1.jpg';
+export { default as lunch2 } from './lunch/lunch2.jpg';
+export { default as lunch3 } from './lunch/lunch3.jpg';
+export { default as lunch4 } from './lunch/lunch4.jpg';
+
+// News
+export { default as news1 } from './news/news1.jpg';
+export { default as news2 } from './news/news2.jpg';
+export { default as news3 } from './news/news3.jpg';
+export { default as news4 } from './news/news4.jpg';
+export { default as news5 } from './news/news5.jpg';
+export { default as news6 } from './news/news6.jpg';
+export { default as news7 } from './news/news7.jpg';
+export { default as news8 } from './news/news8.jpg';
+
+// Team
+export { default as team1 } from './team/team1.jpg';
+export { default as team2 } from './team/team2.jpg';
+export { default as team3 } from './team/team3.jpg';
+
+// Slide
+export { default as slide1 } from './slide/slide1.jpg';
+export { default as slide2 } from './slide/slide2.jpg';
+export { default as slide3 } from './slide/slide3.jpg';
+
+export { default as heroEmail } from './charles-deluvio-FdDkfYFHqe4-unsplash.jpg';
+
+
+
+
+
+
+
+
+
